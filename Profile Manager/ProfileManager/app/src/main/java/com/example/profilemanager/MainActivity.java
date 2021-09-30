b7663c563c9c1034a4b662be139efacffe5009f7
PabloS <jsanc016@uottawa.ca>
2021-09-30 10:34:51 -0400

    ProfileActivity added

--- Profile Manager/ProfileManager/app/src/main/java/com/example/profilemanager/MainActivity.java
+++ Profile Manager/ProfileManager/app/src/main/java/com/example/profilemanager/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.profilemanager;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View;
 import android.content.Intent;
@@ -27,6 +33,60 @@ public class MainActivity extends AppCompatActivity {
         startActivity(mapIntent);
     }
 
+    ActivityResultLauncher<Intent> profileActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == Activity.RESULT_OK){
+                // There are no request codes
+                Intent data = result.getData();
+
+                ImageView avatarImage = (ImageView) findViewById(R.id.avatar);
+                String drawableName = "flag_02";
+
+                switch(data.getIntExtra("imageID",R.id.flagid00)){
+                    case R.id.flagid00:
+                        drawableName = "flag_00";
+                        break;
+                    case R.id.flagid01:
+                        drawableName = "flag_01";
+                        break;
+                    case R.id.flagid02:
+                        drawableName = "flag_02";
+                        break;
+                    case R.id.flagid03:
+                        drawableName = "flag_03";
+                        break;
+                    case R.id.flagid04:
+                        drawableName = "flag_04";
+                        break;
+                    case R.id.flagid05:
+                        drawableName = "flag_05";
+                        break;
+                    case R.id.flagid06:
+                        drawableName = "flag_06";
+                        break;
+                    case R.id.flagid07:
+                        drawableName = "flag_07";
+                        break;
+                    case R.id.flagid08:
+                        drawableName = "flag_08";
+                        break;
+                    default:
+                        drawableName = "flag_09";
+                        break;
+                }
+                int resID = getResources().getIdentifier(drawableName, "drawable",getPackageName());
+                avatarImage.setImageResource(resID);
+            }
+        }
+
+    });
+
+    public void OnSetAvatarButton(View view){
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        profileActivityResultLauncher.launch(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
         googleMaps = findViewById(R.id.googleMaps);
         teamName = findViewById(R.id.teamName);
         teamAddress = findViewById(R.id.teamAddress);
+        avatar = findViewById(R.id.avatar);
 
         googleMaps.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,11 +107,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OnSetAvatarButton(v);
+            }
+        });
+
     }
 
 
 
 
+    ImageView avatar;
     Button googleMaps;
     TextView title, helperName, helperAddress;
     EditText teamName, teamAddress;
--- Profile Manager/ProfileManager/app/src/main/java/com/example/profilemanager/ProfileActivity.java
+++ Profile Manager/ProfileManager/app/src/main/java/com/example/profilemanager/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.profilemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -11,6 +13,20 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        flagid_00 = findViewById(R.id.flagid00);
+        flagid_01 = findViewById(R.id.flagid01);
+        flagid_02 = findViewById(R.id.flagid02);
+        flagid_03 = findViewById(R.id.flagid03);
+        flagid_04 = findViewById(R.id.flagid04);
+        flagid_05 = findViewById(R.id.flagid05);
+        flagid_06 = findViewById(R.id.flagid06);
+        flagid_07 = findViewById(R.id.flagid07);
+        flagid_08 = findViewById(R.id.flagid08);
+        description = findViewById(R.id.description);
+
 
     }
+
+    ImageView flagid_00, flagid_01, flagid_02, flagid_03, flagid_04, flagid_05, flagid_06, flagid_07, flagid_08;
+    TextView description;
 }
