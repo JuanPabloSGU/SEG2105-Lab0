47bf445a7028f21894ceebe92a5d122fca08bebe
PabloS <jsanc016@uottawa.ca>
2021-11-04 09:08:01 -0400

    extracted lab

---
+++ DataBaseLab/DataBaseLab/app/src/main/java/com/example/databaselab/Product.java
@@ -0,0 +1,31 @@
+package com.example.databaselab;
+
+public class Product {
+    private int _id;
+    private String _productname;
+    private double _price;
+
+    // Constructors
+    public Product(){
+
+    }
+
+    public Product(int id, String productname, double price){
+        _id = id;
+        _productname = productname;
+        _price = price;
+    }
+
+    public Product(String productname, double price){
+        _productname = productname;
+        _price = price;
+    }
+
+    // setters and getters
+    public void setID(int id){_id = id;}
+    public int getID(){return _id;}
+    public void setProductName(String productName){_productname = productName;}
+    public String getProductName(){return _productname;}
+    public void setPrice(double price){_price = price;}
+    public double getPrice(){return _price;}
+}
